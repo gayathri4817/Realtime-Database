9abbea48a7acb5ac1dae4f5f376f285c5e1ce8e1
gayathri4817 <78090580+gayathri4817@users.noreply.github.com>
2021-04-14 20:00:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+var ball;
+var DB = firebase.database()
+var position;
+function setup(){
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    position = DB.ref("ball/position");
+    position.on("value", readPosition, showError);
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+   position.set({
+       x:ball.x+x, 
+       y:ball.y+y
+   })
+}
+
+function readPosition(data){
+var pos = data.val()
+ball.x = pos.x;
+ball.y = pos.y;
+}
+
+function showError(Error){
+console.log(Error);
+}
